07c296d7d528df058774fbc50bd18e5aeca8c858
kasraJannati <kasra.ja1991@gmail.com>
2024-04-30 23:30:22 -0400

    feat: add tic tac toe game

--- src/App.js
+++ src/App.js
@@ -1,3 +1,80 @@
-export default function Square() {
-  return <button className="square">X</button>;
+// Toturial purpose only
+// Author: @kasraJannati
+// Source: https://reactjs.org/tutorial/tutorial.html
+import { useState } from "react";
+
+function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
 }
+export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null)); // Creates an array of 9 nulls
+
+  function handleClick(i) {
+    if (squares[i] || calculateWinner(squares)) return;
+    const nextSquares = squares.slice(); // Creates a copy of the squares array
+    nextSquares[i] = xIsNext ? "X" : "O";
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
+  // When you were passing onSquareClick={handleClick}, you were passing the handleClick function down as a prop.
+  // You were not calling it! But now you are calling that function right away—notice the parentheses in handleClick(0)—and that’s why it runs too early.
+  // You don’t want to call handleClick until the user clicks!
+  // You could fix this by creating a function like handleFirstSquareClick that calls handleClick(0), a function like handleSecondSquareClick that calls handleClick(1), and so on.
+  // You would pass(rather than call) these functions down as props like onSquareClick = { handleFirstSquareClick }.This would solve the infinite loop.
+
+  const winner = calculateWinner(squares);
+  let status;
+  status = winner
+    ? "Winner: " + winner
+    : "Next player: " + (xIsNext ? "X" : "O");
+
+  // React fragment <>  </> is used to return multiple elements
+  return (
+    <>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </>
+  );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+// JavaScript supports closures which means an inner function (e.g. handleClick) has access to variables and functions defined in a outer function (e.g. Board).
+// The handleClick function can read the squares state and call the setSquares method because they are both defined inside of the Board function.
